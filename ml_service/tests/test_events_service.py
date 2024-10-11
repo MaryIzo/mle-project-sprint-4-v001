90e2d2a9f68540c83723336c03886a3bb90f390d
Maria I <izofatova.mariya@mail.ru>
2024-10-11 09:04:26 +0000

    test recommendations system

--- ml_service/tests/test_events_service.py
+++ ml_service/tests/test_events_service.py
@@ -4,7 +4,7 @@ events_store_url = "http://127.0.0.1:8020"
 
 headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 
-user_id = 1337055
+user_id = 1291248
 items_for_put = [53404, 33311009, 178529, 35505245, 795836] # из файла recommendations.ipynb
 params_to_get = {"user_id": user_id, "k": 3}
 
--- ml_service/tests/test_recommendations_service.py
+++ ml_service/tests/test_recommendations_service.py
@@ -27,9 +27,6 @@ headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 
 # --------------------добавление событий в онлайн рекомендации---------------
 # params = {"user_id": 1291248, "item_id": 17245}
-
-
-
 # resp = requests.post(events_store_url + "/put", headers=headers, params=params)
 
 # ------------------достаем события из онлайн рекомендаций-----------
@@ -59,23 +56,33 @@ headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 
 # ----------------------- блендинг рекомендаций -------------------
 
-user_id = 1291250
-event_item_ids =  [7144, 16299, 5907, 18135]
+user_id = 1000006
+event_item_ids =  [53404, 33311009, 178529, 35505245, 795836] 
+params = {"user_id": user_id, 'k': 5}
 
 for event_item_id in event_item_ids:
+    print(f'post to event store {event_item_id}')
     resp = requests.post(events_store_url + "/put", 
                          headers=headers, 
                          params={"user_id": user_id, "item_id": event_item_id})
 
-params = {"user_id": 1291250, 'k': 10}
-resp_offline = requests.post(recommendations_url + "/recommendations_offline", headers=headers, params=params)
-resp_online = requests.post(recommendations_url + "/recommendations_online", headers=headers, params=params)
-resp_blended = requests.post(recommendations_url + "/recommendations", headers=headers, params=params)
+print('Reccomendations offline ...')
+resp_offline = requests.post(
+    recommendations_url + "/recommendations_offline", headers=headers, params=params
+    )
+print('Reccomendations online ...')
+resp_online = requests.post(
+    recommendations_url + "/recommendations_online", headers=headers, params=params
+    )
+print('Reccomendations offline ...')
+resp_blended = requests.post(
+    recommendations_url + "/recommendations", headers=headers, params=params
+    )
 
 recs_offline = resp_offline.json()["recs"]
 recs_online = resp_online.json()["recs"]
 recs_blended = resp_blended.json()["recs"]
 
-print(recs_offline)
-print(recs_online)
-print(recs_blended)
+print('Reccomendations offline: ', recs_offline)
+print('Reccomendations online: ', recs_online)
+print('Recommendations blended: ', recs_blended)
