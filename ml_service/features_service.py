bdfb22897b24e2acdd7d420e7db1e3ab19d9179b
Maria I <izofatova.mariya@mail.ru>
2024-10-13 16:55:00 +0000

    refactoring

--- ml_service/features_service.py
+++ ml_service/features_service.py
@@ -4,37 +4,9 @@ from contextlib import asynccontextmanager
 import pandas as pd
 from fastapi import FastAPI
 
+from recommendations import SimilarItems
+
 logger = logging.getLogger("uvicorn.error")
-
-class SimilarItems:
-
-    def __init__(self):
-
-        self._similar_items = None
-
-    def load(self, path, **kwargs):
-        """
-        Загружаем данные из файла
-        """
-        
-        logger.info(f"Loading data, type: {type}")
-        self._similar_items = pd.read_parquet(path)
-        self._similar_items = self._similar_items.sort_values(by='score', ascending=False)
-        logger.info(f"Loaded")
-
-    def get(self, item_id: int, k: int = 10):
-        """
-        Возвращает список похожих объектов
-        """
-        try:
-            df = self._similar_items
-            i2i = df[df["item_id_1"] == item_id].head(k)
-            i2i = i2i[["item_id_2", "score"]].to_dict("list")
-        except KeyError:
-            logger.error("No recommendations found")
-            i2i = {"item_id_2": [], "score": {}}
-
-        return i2i
 
 sim_items_store = SimilarItems()
 
--- ml_service/recommendations.py
+++ ml_service/recommendations.py
@@ -45,3 +45,56 @@ class Recommendations:
         logger.info("Stats for recommendations")
         for name, value in self._stats.items():
             logger.info(f"{name:<30} {value} ")
+
+class EventStore:
+
+    def __init__(self, max_events_per_user=10):
+
+        self.events = {}
+        self.max_events_per_user = max_events_per_user
+
+    def put(self, user_id, item_id):
+        """
+        Сохраняет событие
+        """
+
+        user_events = self.events.get(user_id, [])
+        self.events[user_id] = [item_id] + user_events[: self.max_events_per_user]
+
+    def get(self, user_id, k):
+        """
+        Возвращает события для пользователя
+        """
+        user_events = self.events[user_id][:k]
+
+        return user_events
+    
+class SimilarItems:
+
+    def __init__(self):
+
+        self._similar_items = None
+
+    def load(self, path, **kwargs):
+        """
+        Загружаем данные из файла
+        """
+        
+        logger.info(f"Loading data, type: {type}")
+        self._similar_items = pd.read_parquet(path)
+        self._similar_items = self._similar_items.sort_values(by='score', ascending=False)
+        logger.info(f"Loaded")
+
+    def get(self, item_id: int, k: int = 10):
+        """
+        Возвращает список похожих объектов
+        """
+        try:
+            df = self._similar_items
+            i2i = df[df["item_id_1"] == item_id].head(k)
+            i2i = i2i[["item_id_2", "score"]].to_dict("list")
+        except KeyError:
+            logger.error("No recommendations found")
+            i2i = {"item_id_2": [], "score": {}}
+
+        return i2i
